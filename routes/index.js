2d4c9aaf83ec30a61f004f13770b02288d133c70
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2013-08-02 14:26:18 -0700

    fuckeverything

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,24 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'meanie' });
+  res.render('index', { title: 'meanie',
+    todos: [
+  {
+    description:"buy eggs",
+    due: new Date(new Date().getTime() + 24 * 60 * 60 * 1000),
+    done: false
+  },
+  {
+    description:"buy more eggs",
+    due: new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000),
+    done: false
+  },
+  {
+    description:"buy even more eggs",
+    due: new Date(new Date().getTime() + 7 * 24 * 60 * 60 * 1000),
+    done: true
+     
+  }
+    ]
+  });
 };
