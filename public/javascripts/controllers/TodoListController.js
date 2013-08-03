3100b8fa0e8e897600322a062a80a2b2c11146e1
mohsenil85 <mohsenil85@gmail.com>
2013-08-03 15:23:45 -0700

    working

---
+++ public/javascripts/controllers/TodoListController.js
@@ -0,0 +1,9 @@
+function TodoListController($scope){
+  $scope.todos = [];
+  $scope.doneFilter = { done: true };
+  $scope.notDoneFilter = { done: false };
+
+  $scope.setTodos = function(todos) {
+    $scope.todos = todos;
+  };
+}
